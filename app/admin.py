af14056e2993bd6da4dee5e1ade485ed00ff72b6
FabianBugueno <fa.bugueno@duocuc.cl>
2025-05-15 17:57:21 -0400

    Personalizacion Django Incompleto

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Marca, Producto
 # Register your models here.
 
+class ProductoAdmin(admin.ModelAdmin):  
+    list_display = ('nombre', 'precio', 'nuevo', 'marca')
+    list_editable = ('precio',) 
+    search_fields = ('nombre',)
+    list_filter = ('nuevo', 'marca')
+
 admin.site.register(Marca)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)    
