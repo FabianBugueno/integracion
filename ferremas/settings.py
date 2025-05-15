612cfda5d708a8227aed048e32e0c8ef2c9cde6b
FabianBugueno <fa.bugueno@duocuc.cl>
2025-05-15 19:49:12 -0400

    Listado home

--- ferremas/settings.py
+++ ferremas/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'app.apps.FerremasssConfig',
     'colorfield',
+    'django.contrib.humanize',
 ]
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
